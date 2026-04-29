7225117ae4819ff08dbbdc5b644c3175aab09494
umg <umg@JOC-LAB-1A4.labjoc.local>
2026-04-28 18:55:18 -0600

    se creo paquete modelo

---
+++ src/modelo/libro.java
@@ -0,0 +1,27 @@
+
+package modelo;
+import modelo.libro;
+import conexion.CreateConnection;
+import dao.librodao;
+import controller.librocontroller;
+import vista.librovista;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.*;
+public class libro {
+    int id;
+    String titulo;
+    String autor;
+    int year;
+    int paginas;
+    String genero;
+    String editorial;
+    String descripcion;
+    String idioma;
+    String estudiante;
+    int Stock;
+    
+    
+    
+}
