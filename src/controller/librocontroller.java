9ca915a3d098c7b00298b848286481641172d022
umg <umg@JOC-LAB-1A4.labjoc.local>
2026-04-28 20:31:36 -0600

    se termino controller

--- src/controller/librocontroller.java
+++ src/controller/librocontroller.java
@@ -3,11 +3,37 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package controller;
+import modelo.libro;
+import conexion.CreateConnection;
+import dao.librodao;
+import controller.librocontroller;
+import vista.librovista;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.*;
 
-/**
- *
- * @author umg
- */
 public class librocontroller {
     
+    private final librodao dao = new librodao();
+    
+    public List<libro> verlibro(){
+        
+        return dao.obtenerTodos();
+        
+        
+    }
+    public void guardarlibro(String nombre, String autor, int year, int paginas, String genero, String editorial, String descripcion, String idioma, String estudiante, int stock ){
+    libro lib = new libro(0, nombre, autor, year, paginas, genero, editorial, descripcion, idioma, estudiante);
+    dao.guardar(lib);
+    }
+    public void actualizarlibro(int id,String nombre, String autor, int year, int paginas, String genero, String editorial, String descripcion, String idioma, String estudiante, int stock ){
+    libro lib = new libro(id, nombre, autor, year, paginas, genero, editorial, descripcion, idioma, estudiante);
+    dao.actualizar(lib);
+    }
+    public void eliminarlibro(int id){
+        dao.eliminar(id);
+    }
 }
